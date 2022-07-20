662cd9e7e741384143dd38eea34ad6ac977e0af1
Matthew Guizar <matthewrguizar@gmail.com>
2022-07-20 09:45:53 -0700

    interactivity

--- Main.java
+++ Main.java
@@ -13,12 +13,42 @@ public class Main {
             loadContacts("contacts.txt");
             System.out.println("contacts loaded\n\n");
             System.out.println(manager);
+            manageContacts();
          }catch (FileNotFoundException e ){
             System.out.println(e.getMessage());
         }
     }
 
-
+    public static void manageContacts() {
+        Scanner scan = new Scanner(System.in);
+        while (true) {
+            System.out.println("Would you like to \n\ta) add another contact\n\tb) remove a contact \n\tc) exit");
+           String response = scan.nextLine();
+           if(response.equalsIgnoreCase("a")) {
+                System.out.print("\tName: ");
+                String name = scan.nextLine();
+                System.out.print("\tPhone Number: ");
+                String phoneNumber = scan.nextLine();
+                System.out.println("\tBirth date: ");
+                String birthDate = scan.nextLine();
+                try {
+                    manager.addContact(new Contact(name, phoneNumber, birthDate));
+                } catch (ParseException e) {
+                   
+                    System.out.println(e.getMessage());
+                } finally {
+                    System.out.println("\n\nUpdated Contacts\n\n" + manager);
+                }
+           } else if(response.equalsIgnoreCase("b")) {
+               System.out.println("\nWho would you like to remove?");
+               manager.removeContact(scan.nextLine());
+               System.out.println("\n\nUpdated Contacts\n\n" + manager); 
+           } else {
+                break;
+           }
+        }
+        scan.close();
+    }
 
     public static void loadContacts(String fileName) throws FileNotFoundException{
         FileInputStream file = new FileInputStream(fileName);
